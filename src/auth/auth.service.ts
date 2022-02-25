256953132ca013d8cce4cd279745cc21a4d70aa3
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-02-24 23:53:54 -0600

    yes 2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
-@Injectable({})
+import { PrismaService } from 'src/prisma/prisma.service';
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  
+  
   signin() {
+    PrismaService;
     return 'fuck u bitch';
   }
+  
   signup() {
     return ' yea bitch fuck u';
   }
