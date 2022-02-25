d9df9d2e16af2ab5e1431f14f8dcc76070fcaed5
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-02-24 23:28:23 -0600

    yes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 @Injectable({})
-export class AuthService{
-
-    signin(){
-        return 'fuck u bitch'
-    }
-    signup(){
-        return ' yea bitch fuck u'
-    }
+export class AuthService {
+  signin() {
+    return 'fuck u bitch';
+  }
+  signup() {
+    return ' yea bitch fuck u';
+  }
 }
