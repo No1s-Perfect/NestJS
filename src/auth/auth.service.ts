25e2f7ac26ba7d27e7c9c65e19ba2afa6c69c4ca
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-02-27 23:09:14 -0600

    easdasd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  
-  
-  signin() {
-    PrismaService;
-    return 'fuck u bitch';
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Email already in use');
+        }
+        throw e;
+      }
+    }
   }
-  
-  signup() {
-    return ' yea bitch fuck u';
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Email not found');
+    const pMatch = await argon.verify(user.hash, dto.password);
+    if (!pMatch) throw new ForbiddenException('Password does not match');
+
+    delete user.hash;
+    return user;
   }
 }
